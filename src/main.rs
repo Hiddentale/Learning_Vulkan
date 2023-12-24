254573de5bb2d3eafff41ee4f7f852d1f91fbda4
Seloria <johndoe@example.com>
2023-12-24 23:02:51 +0100

    Finished up a barebones version of the graphics pipeline.

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,30 @@ unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
 
     let vertex_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::VERTEX).module(vertex_shader_module).name(b"main\0");
     let fragment_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::FRAGMENT).module(fragment_shader_module).name(b"main\0");
-    // ...
+    let vertex_input_state = vk::PipelineVertexInputStateCreateInfo::builder();
+    let input_assembly_state = vk::PipelineInputAssemblyStateCreateInfo::builder().topology(vk::PrimitiveTopology::TRIANGLE_LIST).primitive_restart_enable(false);
+    let viewport = vk::Viewport::builder().x(0.0).y(0.0).width(data.swapchain_extent.width as f32).height(data.swapchain_extent.height as f32).min_depth(0.0).max_depth(1.0);
+    let scissor = vk::Rect2D::builder().offset(vk::Offset2D { x: 0, y: 0 }).extent(data.swapchain_extent);
+    let viewports = &[viewport];
+    let scissors = &[scissor];
+    let viewport_state = vk::PipelineViewportStateCreateInfo::builder().viewports(viewports).scissors(scissors);
+    let rasterization_state = vk::PipelineRasterizationStateCreateInfo::builder().depth_clamp_enable(false).rasterizer_discard_enable(false).polygon_mode(vk::PolygonMode::FILL)
+        .line_width(1.0).cull_mode(vk::CullModeFlags::BACK).front_face(vk::FrontFace::CLOCKWISE).depth_bias_enable(false);
+    let multisample_state = vk::PipelineMultisampleStateCreateInfo::builder().sample_shading_enable(false).rasterization_samples(vk::SampleCountFlags::_1);
+    let attachment = vk::PipelineColorBlendAttachmentState::builder().color_write_mask(vk::ColorComponentFlags::all()).blend_enable(true).src_color_blend_factor(vk::BlendFactor::SRC_ALPHA)
+        .dst_color_blend_factor(vk::BlendFactor::ONE_MINUS_SRC_ALPHA).color_blend_op(vk::BlendOp::ADD).src_alpha_blend_factor(vk::BlendFactor::ONE).dst_alpha_blend_factor(vk::BlendFactor::ZERO).alpha_blend_op(vk::BlendOp::ADD);
+    let attachments = &[attachment];
+    let color_blend_state = vk::PipelineColorBlendStateCreateInfo::builder().logic_op_enable(false).logic_op(vk::LogicOp::COPY).attachments(attachments).blend_constants([0.0, 0.0, 0.0, 0.0]);
+    let layout_info = vk::PipelineLayoutCreateInfo::builder();
+
+    data.pipeline_layout = device.create_pipeline_layout(&layout_info, None)?;
+
+    let stages = &[vertex_stage, fragment_stage];
+    let info = vk::GraphicsPipelineCreateInfo::builder().stages(stages).vertex_input_state(&vertex_input_state).input_assembly_state(&input_assembly_state).viewport_state(&viewport_state)
+        .rasterization_state(&rasterization_state).multisample_state(&multisample_state).color_blend_state(&color_blend_state).layout(data.pipeline_layout).render_pass(data.render_pass).subpass(0);
+
+    data.pipeline = device.create_graphics_pipelines(vk::PipelineCache::null(), &[info], None)?.0[0];
+
 
     device.destroy_shader_module(vertex_shader_module, None);
     device.destroy_shader_module(fragment_shader_module, None);
@@ -111,6 +134,8 @@ impl VulkanApplication {
         let device = create_logical_device(&entry,&instance, &mut data)?;
         create_swapchain(window, &instance, &device, &mut data)?;
         create_swapchain_image_views(&device, &mut data)?;
+        create_render_pass(&instance, &device, &mut data)?;
+        create_pipeline(&device, &mut data)?;
         Ok(Self {entry, instance, data, device})
     }
 
@@ -121,6 +146,9 @@ impl VulkanApplication {
 
     /// Destroys our Vulkan app.
     unsafe fn destroy(&mut self) {
+        self.device.destroy_pipeline(self.data.pipeline, None);
+        self.device.destroy_pipeline_layout(self.data.pipeline_layout, None);
+        self.device.destroy_render_pass(self.data.render_pass, None);
         self.data.swapchain_image_views.iter().for_each(|v| self.device.destroy_image_view(*v, None));
         self.device.destroy_swapchain_khr(self.data.swapchain, None);
         self.device.destroy_device(None);
@@ -130,6 +158,21 @@ impl VulkanApplication {
         self.instance.destroy_surface_khr(self.data.surface, None);
         self.instance.destroy_instance(None);
     }
+}
+
+
+unsafe fn create_render_pass(instance: &Instance, device: &Device, data: &mut AppData) -> Result<()> {
+    let color_attachment = vk::AttachmentDescription::builder().format(data.swapchain_format).samples(vk::SampleCountFlags::_1).load_op(vk::AttachmentLoadOp::CLEAR).store_op(vk::AttachmentStoreOp::STORE)
+        .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE).stencil_store_op(vk::AttachmentStoreOp::DONT_CARE).initial_layout(vk::ImageLayout::UNDEFINED).final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+    let color_attachment_ref = vk::AttachmentReference::builder().attachment(0).layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+    let color_attachments = &[color_attachment_ref];
+    let subpass = vk::SubpassDescription::builder().pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS).color_attachments(color_attachments);
+    let attachments = &[color_attachment];
+    let subpasses = &[subpass];
+    let info = vk::RenderPassCreateInfo::builder().attachments(attachments).subpasses(subpasses);
+
+    data.render_pass = device.create_render_pass(&info, None)?;
+    Ok(())
 }
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
@@ -143,7 +186,10 @@ struct AppData {
     swapchain_extent: vk::Extent2D,
     swapchain: vk::SwapchainKHR,
     swapchain_images: Vec<vk::Image>,
-    swapchain_image_views: Vec<vk::ImageView>
+    swapchain_image_views: Vec<vk::ImageView>,
+    render_pass: vk::RenderPass,
+    pipeline_layout: vk::PipelineLayout,
+    pipeline: vk::Pipeline
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
