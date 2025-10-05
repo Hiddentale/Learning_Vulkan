60fc1bd34ad08a4cc70876db77141cd5361c5afd
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-10-05 12:28:17 +0200

    Figuring out how to acquire possible memory type pointer index

--- src/graphical_core/pipeline.rs
+++ src/graphical_core/pipeline.rs
@@ -1,43 +1,88 @@
-use vulkanalia::{Device, vk};
-use vulkanalia::vk::{DeviceV1_0, Handle, HasBuilder};
 use crate::graphical_core::shaders::create_shader_module;
 use crate::graphical_core::vulkan_object::VulkanApplicationData;
+use vulkanalia::vk::{DeviceV1_0, Handle, HasBuilder};
+use vulkanalia::{vk, Device};
 
-pub unsafe fn create_pipeline(device: &Device, data: &mut VulkanApplicationData) -> anyhow::Result<()> {
+pub unsafe fn create_pipeline(vulkan_logical_device: &Device, data: &mut VulkanApplicationData) -> anyhow::Result<()> {
     let vertex_shader = include_bytes!("../shaders/shader.vert.spv");
     let fragment_shader = include_bytes!("../shaders/shader.frag.spv");
 
-    let vertex_shader_module = create_shader_module(device, &vertex_shader[..])?;
-    let fragment_shader_module = create_shader_module(device, &fragment_shader[..])?;
+    let vertex_shader_module = create_shader_module(vulkan_logical_device, &vertex_shader[..])?;
+    let fragment_shader_module = create_shader_module(vulkan_logical_device, &fragment_shader[..])?;
 
-    let vertex_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::VERTEX).module(vertex_shader_module).name(b"main\0");
-    let fragment_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::FRAGMENT).module(fragment_shader_module).name(b"main\0");
+    let vertex_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::VERTEX)
+        .module(vertex_shader_module)
+        .name(b"main\0");
+    let fragment_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::FRAGMENT)
+        .module(fragment_shader_module)
+        .name(b"main\0");
     let vertex_input_state = vk::PipelineVertexInputStateCreateInfo::builder();
-    let input_assembly_state = vk::PipelineInputAssemblyStateCreateInfo::builder().topology(vk::PrimitiveTopology::TRIANGLE_LIST).primitive_restart_enable(false);
-    let viewport = vk::Viewport::builder().x(0.0).y(0.0).width(data.swapchain_accepted_images_width_and_height.width as f32).height(data.swapchain_accepted_images_width_and_height.height as f32).min_depth(0.0).max_depth(1.0);
-    let scissor = vk::Rect2D::builder().offset(vk::Offset2D { x: 0, y: 0 }).extent(data.swapchain_accepted_images_width_and_height);
+    let input_assembly_state = vk::PipelineInputAssemblyStateCreateInfo::builder()
+        .topology(vk::PrimitiveTopology::TRIANGLE_LIST)
+        .primitive_restart_enable(false);
+    let viewport = vk::Viewport::builder()
+        .x(0.0)
+        .y(0.0)
+        .width(data.swapchain_accepted_images_width_and_height.width as f32)
+        .height(data.swapchain_accepted_images_width_and_height.height as f32)
+        .min_depth(0.0)
+        .max_depth(1.0);
+    let scissor = vk::Rect2D::builder()
+        .offset(vk::Offset2D { x: 0, y: 0 })
+        .extent(data.swapchain_accepted_images_width_and_height);
     let viewports = &[viewport];
     let scissors = &[scissor];
     let viewport_state = vk::PipelineViewportStateCreateInfo::builder().viewports(viewports).scissors(scissors);
-    let rasterization_state = vk::PipelineRasterizationStateCreateInfo::builder().depth_clamp_enable(false).rasterizer_discard_enable(false).polygon_mode(vk::PolygonMode::FILL)
-        .line_width(1.0).cull_mode(vk::CullModeFlags::BACK).front_face(vk::FrontFace::CLOCKWISE).depth_bias_enable(false);
-    let multisample_state = vk::PipelineMultisampleStateCreateInfo::builder().sample_shading_enable(false).rasterization_samples(vk::SampleCountFlags::_1);
-    let attachment = vk::PipelineColorBlendAttachmentState::builder().color_write_mask(vk::ColorComponentFlags::all()).blend_enable(true).src_color_blend_factor(vk::BlendFactor::SRC_ALPHA)
-        .dst_color_blend_factor(vk::BlendFactor::ONE_MINUS_SRC_ALPHA).color_blend_op(vk::BlendOp::ADD).src_alpha_blend_factor(vk::BlendFactor::ONE).dst_alpha_blend_factor(vk::BlendFactor::ZERO).alpha_blend_op(vk::BlendOp::ADD);
+    let rasterization_state = vk::PipelineRasterizationStateCreateInfo::builder()
+        .depth_clamp_enable(false)
+        .rasterizer_discard_enable(false)
+        .polygon_mode(vk::PolygonMode::FILL)
+        .line_width(1.0)
+        .cull_mode(vk::CullModeFlags::BACK)
+        .front_face(vk::FrontFace::CLOCKWISE)
+        .depth_bias_enable(false);
+    let multisample_state = vk::PipelineMultisampleStateCreateInfo::builder()
+        .sample_shading_enable(false)
+        .rasterization_samples(vk::SampleCountFlags::_1);
+    let attachment = vk::PipelineColorBlendAttachmentState::builder()
+        .color_write_mask(vk::ColorComponentFlags::all())
+        .blend_enable(true)
+        .src_color_blend_factor(vk::BlendFactor::SRC_ALPHA)
+        .dst_color_blend_factor(vk::BlendFactor::ONE_MINUS_SRC_ALPHA)
+        .color_blend_op(vk::BlendOp::ADD)
+        .src_alpha_blend_factor(vk::BlendFactor::ONE)
+        .dst_alpha_blend_factor(vk::BlendFactor::ZERO)
+        .alpha_blend_op(vk::BlendOp::ADD);
     let attachments = &[attachment];
-    let color_blend_state = vk::PipelineColorBlendStateCreateInfo::builder().logic_op_enable(false).logic_op(vk::LogicOp::COPY).attachments(attachments).blend_constants([0.0, 0.0, 0.0, 0.0]);
+    let color_blend_state = vk::PipelineColorBlendStateCreateInfo::builder()
+        .logic_op_enable(false)
+        .logic_op(vk::LogicOp::COPY)
+        .attachments(attachments)
+        .blend_constants([0.0, 0.0, 0.0, 0.0]);
     let layout_info = vk::PipelineLayoutCreateInfo::builder();
 
-    data.pipeline_layout = device.create_pipeline_layout(&layout_info, None)?;
+    data.pipeline_layout = vulkan_logical_device.create_pipeline_layout(&layout_info, None)?;
 
     let stages = &[vertex_stage, fragment_stage];
-    let info = vk::GraphicsPipelineCreateInfo::builder().stages(stages).vertex_input_state(&vertex_input_state).input_assembly_state(&input_assembly_state).viewport_state(&viewport_state)
-        .rasterization_state(&rasterization_state).multisample_state(&multisample_state).color_blend_state(&color_blend_state).layout(data.pipeline_layout).render_pass(data.render_pass).subpass(0);
+    let info = vk::GraphicsPipelineCreateInfo::builder()
+        .stages(stages)
+        .vertex_input_state(&vertex_input_state)
+        .input_assembly_state(&input_assembly_state)
+        .viewport_state(&viewport_state)
+        .rasterization_state(&rasterization_state)
+        .multisample_state(&multisample_state)
+        .color_blend_state(&color_blend_state)
+        .layout(data.pipeline_layout)
+        .render_pass(data.render_pass)
+        .subpass(0);
 
-    data.pipeline = device.create_graphics_pipelines(vk::PipelineCache::null(), &[info], None)?.0[0];
+    data.pipeline = vulkan_logical_device
+        .create_graphics_pipelines(vk::PipelineCache::null(), &[info], None)?
+        .0[0];
 
-
-    device.destroy_shader_module(vertex_shader_module, None);
-    device.destroy_shader_module(fragment_shader_module, None);
+    vulkan_logical_device.destroy_shader_module(vertex_shader_module, None);
+    vulkan_logical_device.destroy_shader_module(fragment_shader_module, None);
     Ok(())
 }
--- src/graphical_core/vertex_buffer.rs
+++ src/graphical_core/vertex_buffer.rs
@@ -14,11 +14,37 @@ Plan:
     Tell the GPU how to interpret the data
  */
 
-use vulkanalia::{vk::DeviceV1_0, Device, vk};
+use anyhow;
+use vulkanalia::{vk::{self, DeviceV1_0}, Device, Instance};
+use crate::graphical_core::vulkan_object::VulkanApplicationData;
 
 //#[repr(C)]
 //#[derive(Copy, Clone)]
-unsafe fn temp(device: &Device) {
-    let buffer_create_info = &vk::BufferCreateInfo {};
-    let buffer = device.create_buffer(buffer_create_info,);
+unsafe fn temp(vulkan_logical_device: &Device, instance: &Instance, vulkan_application_data: &mut VulkanApplicationData) -> anyhow::Result<()> {
+
+    let vertex_buffer_create_info = &vk::BufferCreateInfo {
+        size: 12, // Change this to actual vertex data list size calculation: vertices.len() × size_of::<Vertex>()
+        usage: vk::BufferUsageFlags::VERTEX_BUFFER,
+        sharing_mode: vk::SharingMode::EXCLUSIVE,
+        ..Default::default()
+    };
+
+    let vertex_buffer = vulkan_logical_device.create_buffer(vertex_buffer_create_info, None)?;
+    
+    let v_buffer_mem_requirement = vulkan_logical_device.get_buffer_memory_requirements(vertex_buffer);
+
+    let v_buffer_memory_type = find_memory_type(
+        memory_properties: instance.get_physical_device_memory_properties(physical_device),
+        type_filter: v_buffer_mem_requirement,
+        properties: vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
+    );
+    Ok(())
+}
+
+fn find_memory_type(
+    memory_properties: &vk::PhysicalDeviceMemoryProperties,
+    type_filter: u32,
+    properties: vk::MemoryPropertyFlags,
+) -> Option<u32> {
+    Ok
 }
