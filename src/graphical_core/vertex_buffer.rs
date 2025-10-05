b2c0720e946b86cc964e2724b5a7179af62d44c0
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-10-05 18:58:11 +0200

    Working on finding memory type

--- src/graphical_core/vertex_buffer.rs
+++ src/graphical_core/vertex_buffer.rs
@@ -13,38 +13,61 @@ Plan:
     Bind that buffer when  ready to draw
     Tell the GPU how to interpret the data
  */
-
-use anyhow;
-use vulkanalia::{vk::{self, DeviceV1_0}, Device, Instance};
+/* */
 use crate::graphical_core::vulkan_object::VulkanApplicationData;
+use anyhow;
+use vulkanalia::{
+    vk::{self, DeviceV1_0, InstanceV1_0},
+    Device, Instance,
+};
 
-//#[repr(C)]
-//#[derive(Copy, Clone)]
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    pos: [f32; 2],
+    color: [f32; 3],
+}
+
+const VERTICES: [Vertex; 3] = [
+    Vertex {
+        pos: [0.0, -0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        pos: [0.5, 0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        pos: [-0.5, 0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+];
+
 unsafe fn temp(vulkan_logical_device: &Device, instance: &Instance, vulkan_application_data: &mut VulkanApplicationData) -> anyhow::Result<()> {
-
     let vertex_buffer_create_info = &vk::BufferCreateInfo {
-        size: 12, // Change this to actual vertex data list size calculation: vertices.len() × size_of::<Vertex>()
+        size: (VERTICES.len() * size_of::<Vertex>()) as u64,
         usage: vk::BufferUsageFlags::VERTEX_BUFFER,
         sharing_mode: vk::SharingMode::EXCLUSIVE,
         ..Default::default()
     };
 
     let vertex_buffer = vulkan_logical_device.create_buffer(vertex_buffer_create_info, None)?;
-    
+
     let v_buffer_mem_requirement = vulkan_logical_device.get_buffer_memory_requirements(vertex_buffer);
 
-    let v_buffer_memory_type = find_memory_type(
-        memory_properties: instance.get_physical_device_memory_properties(physical_device),
-        type_filter: v_buffer_mem_requirement,
-        properties: vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-    );
+    let memory_properties = instance.get_physical_device_memory_properties(vulkan_application_data.physical_device);
+    let type_filter = v_buffer_mem_requirement.memory_type_bits;
+    let desired_properties = vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT;
+
+    let v_buffer_memory_type = find_memory_type(&memory_properties, type_filter, desired_properties);
     Ok(())
 }
 
-fn find_memory_type(
-    memory_properties: &vk::PhysicalDeviceMemoryProperties,
-    type_filter: u32,
-    properties: vk::MemoryPropertyFlags,
-) -> Option<u32> {
-    Ok
+fn find_memory_type(memory_properties: &vk::PhysicalDeviceMemoryProperties, type_filter: u32, properties: vk::MemoryPropertyFlags) -> Option<u32> {
+    let number_of_different_memory_types = memory_properties.memory_type_count;
+    for i in 0..(number_of_different_memory_types - 1) {
+        if (type_filter & (1 << i)) != 0 { // Need to understand this better
+        }
+    }
+    Some(2)
 }
