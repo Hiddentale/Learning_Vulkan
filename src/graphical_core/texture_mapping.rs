bc11928a3cc52ab29c509ff446995dbe99c5a691
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-10-24 18:49:14 +0200

    Trying to create an image

--- src/graphical_core/texture_mapping.rs
+++ src/graphical_core/texture_mapping.rs
@@ -1,20 +1,25 @@
 use crate::graphical_core::buffers::allocate_and_fill_buffer;
 use crate::graphical_core::vulkan_object::VulkanApplicationData;
 use image;
-use vulkanalia::vk::BufferUsageFlags;
-use vulkanalia::{Device, Instance};
+use vulkanalia::vk;
+use vulkanalia::vk::{BufferUsageFlags, MemoryPropertyFlags};
+use vulkanalia::{Device, DeviceV1_0, Instance};
 
-fn load_texture_from_disk(path_to_texture: &str) -> anyhow::Result<Vec<u8>> {
+fn load_texture_from_disk(path_to_texture: &str) -> anyhow::Result<(Vec<u8>, u32, u32)> {
     let texture = image::ImageReader::open(path_to_texture)?.decode()?;
 
+    let width = texture.width();
+    let height = texture.height();
+
     let image_bytes = texture.to_rgba8().into_raw();
-    Ok(image_bytes)
+
+    Ok((image_bytes, width, height))
 }
 
 fn create_staging_buffer(
     image_bytes: Vec<u8>,
-    width: i32,
-    height: i32,
+    width: u32,
+    height: u32,
     vulkan_logical_device: &Device,
     instance: &Instance,
     vulkan_application_data: &mut VulkanApplicationData,
@@ -33,7 +38,26 @@ fn create_staging_buffer(
     Ok(())
 }
 
-fn create_image() {}
+fn create_image(device: &Device, width: u32, height: u32) -> anyhow::Result<vk::Image> {
+    let image_info = vk::ImageCreateInfo {
+        s_type: vk::StructureType::IMAGE_CREATE_INFO,
+        flags: None,
+        next: std::ptr::null(),
+        image_type: vk::ImageType::_2D,
+        format: vk::Format::R8G8B8A8_SRGB,
+        extent: vk::Extent3D { width, height },
+        mip_levels: None,
+        array_layers: None,
+        samples: None,
+        tiling: vk::ImageTiling::OPTIMAL,
+        usage: vk::ImageUsageFlags::TRANSFER_SRC,
+        sharing_mode: None,
+        queue_family_index_count: None,
+        queue_family_indices: None,
+        initial_layout: None,
+    };
+    device.create_image(&image_info, allocator)
+}
 
 fn create_image_view() {}
 
@@ -52,7 +76,7 @@ fn allocate_descriptor_set() {}
 fn update_descriptor_set() {}
 
 pub fn check_working() -> anyhow::Result<()> {
-    let image_bytes = load_texture_from_disk("textures/red_grass.png")?;
+    let (image_bytes, width, height) = load_texture_from_disk("textures/red_grass.png")?;
     let staging_buffer = create_staging_buffer(image_bytes, width, height, vulkan_logical_device, instance, vulkan_application_data);
     Ok(())
 }
