b1ba206e1cb3266b92d1a83fcbfdeeec34d51f4b
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2026-01-25 18:04:15 +0100

    Started on matrix transformations

--- src/graphical_core/camera.rs
+++ src/graphical_core/camera.rs
@@ -1,6 +1,24 @@
 use crate::graphical_core::{buffers::allocate_and_fill_buffer, vulkan_object::VulkanApplicationData};
+use glam::{Mat4, Vec3};
 use vulkanalia::vk;
 use vulkanalia::{vk::BufferUsageFlags, Device, Instance};
+
+/*
+Initialization:
+    Projection = compute_projection(fov, aspect, near, far)
+
+Each frame:
+    if camera_moved:
+        View = compute_view(camera_pos, camera_target, up)
+
+    ViewProjection = Projection × View
+    Send ViewProjection to GPU
+
+    For each object:
+        Model = compute_model(object_pos, object_rotation)
+        Send Model to GPU
+        GPU: gl_Position = ViewProjection × Model × vertex
+ */
 
 fn create_and_fill_uniform_buffer(
     matrices_bytes: Vec<u8>,
@@ -17,8 +35,20 @@ fn create_and_fill_uniform_buffer(
             vulkan_logical_device,
             instance,
             vulkan_application_data,
-            vk::MemoryPropertyFlags::DEVICE_LOCAL | vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL | vk::MemoryPropertyFlags::HOST_VISIBLE,
         )?
     };
     Ok(uniform_buffer)
+}
+
+fn create_projection_matrix(fov_degrees: f32, near: f32, far: f32, width: f32, height:f32) -> anyhow::Result<Mat4> {
+    let fov_radians = fov_degrees.to_radians();
+    let aspect_ratio = width / height;
+    let projection_matrix = Mat4::perspective_rh(fov_radians, aspect_ratio, near, far);
+    Ok(projection_matrix)
+}
+
+fn create_view_matrix() -> anyhow::Result<Mat4> {
+    let view_matrix = Mat4::look_at_rh(eye, center, up);
+    Ok(view_matrix)
 }
